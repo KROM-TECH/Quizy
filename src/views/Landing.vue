8bd9958749856608a200f106647a38c503e72dc0
kromate <akpananthony33@gmail.com>
2020-09-13 17:54:42 +0100

    initial-commit

---
+++ src/views/Landing.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="body">
+      <h1>Quizy</h1>
+      <p><i>What We Offer</i></p>
+      <ul>
+        <li>Create Quiz</li>
+        <li>Monetize your Quiz</li>
+        <li>Set Prices for Quizes</li>
+        <li>Track Your Quizes</li>
+        <li>Play Quizes Anonymously and still get paid</li>
+        <li>View High Scores</li>
+        <li>And so much more</li>
+      </ul>
+      <div>
+        <button>Create an Account</button>
+        <button>Tell a Friend</button>
+      </div>
+    </div>
+</template>
+
+<script>
+  export default {};
+
+  
+</script>
+
+<style scoped>
+p{
+  font-size: 1.5rem;
+  font-weight: 300;
+}
+
+button{
+     padding: 10px;
+    margin: 1rem;
+    background-color: transparent;
+    border: 1px solid #15a2abb0;
+    font-size: 0.9rem;
+    border-radius: 10px;
+    color: #15a2abb0;
+    outline: none;
+}
+button:hover{
+  background-color:#15a2abb0;
+  color: #fff;
+}
+  .body {
+    text-align: center;  
+    padding: 0rem 1rem !important;  
+  }
+  div h1 {
+    font-size: 4rem;
+    font-family: emoji;
+    margin-bottom: 1rem;
+    letter-spacing: 0.8rem;
+    font-variant: all-petite-caps;
+    font-style: italic;
+  }
+
+  ul {
+    list-style: none;
+    letter-spacing: 1px;
+    font-size: 1.2rem;
+  }
+  li{
+    border-radius: 40px;
+    background-color: #ecf0f3;
+    box-shadow: 13px 13px 20px #cbced1,
+                -13px -13px 20px #ffffff;
+    margin: 0.6rem 0rem;
+    padding: 8px;
+    border: 1.7px solid #ddd;
+  }
+  li:hover{
+        box-shadow: inset 13px 13px 20px #cbced1,
+                inset -13px -13px 20px #ffffff;
+  }
+  li:last-child {
+    font-style: italic;
+    margin-top: 0.9rem;
+    font-size: larger;
+    font-weight: 600;
+    background-color: #75c5c557;
+  }
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="body">
+    <div class="login-div">
+      <h1>Quizy</h1>
+      <form>
+      <div class="username"><input type="username" class="user-input" placeholder="Username"></div>
+      <div class="password"><input type="password" class="user-input" placeholder="Password"></div>
+
+      <button class="btn">Login</button>
+      </form>
+
+      <div>
+        <p>Create an acc</p>
+        <p>Forget Password</p>
+      </div>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+h1{
+   font-size: 3.5rem;
+    font-family: emoji;
+    margin-bottom: 1rem;
+    letter-spacing: 0.8rem;
+    font-variant: all-petite-caps;
+    font-style: italic;
+}
+  .body {
+    text-align: center;  
+    padding: 1rem 1rem !important;  
+  }
+.btn{
+  outline:none;
+  border:none;
+  cursor:pointer;
+  font-weight: 700;
+  padding: 1rem;
+    background-color: #ecf0f3;
+    box-shadow: 13px 13px 20px #cbced1,
+                -13px -13px 20px #ffffff;
+  border: 1px solid;
+  border-radius: 10px;
+}
+form{
+  padding: 1rem !important;
+}
+
+form input{
+  border: none;
+  width: 100%;
+  outline: none;
+  background: none;
+  font-size: 18px;
+  color: #555;
+  margin: 20px 10px 20px 5px;
+}
+
+.username, .password{
+  text-align: start;
+  padding:0rem 1rem ;
+  max-width: 30rem;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 30px;
+  border-radius: 25px;
+  box-shadow: inset 8px 8px 8px #cbced1,
+              inset -8px -8px 8px #ffffff;
+}
+.login-div{
+  height: 35rem;
+  border-radius: 40px;
+  background: #ecf0f3;
+     box-shadow: 13px 13px 20px #cbced1,
+                -13px -13px 20px #ffffff;
+}
+
+.title{
+  text-align: center;
+}
+</style>
