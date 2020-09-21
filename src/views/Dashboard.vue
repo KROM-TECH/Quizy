4536d3f1380d448a60a39ff8f47fb311842d55e6
kromate <akpananthony33@gmail.com>
2020-09-21 13:16:03 +0100

    dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="content-wrapper">
+    <div class="row">
+            <div class="col-md-6 col-lg-6 col-xl-3 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="icon-wrap">
+                      <i class="mdi mdi-folder-outline"></i>
+                    </div>
+                    <div class="flex-right-height">
+                      <p class="font-weight-bold mb-1">Total Quiz Created</p>
+                      <h2>12,456 k</h2>
+                      <p class="font-weight-medium p-0"><span class="text-success mr-2">+1.2%</span><span>Last month</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-xl-3 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="icon-wrap">
+                      <i class="mdi mdi-clipboard-text"></i>
+                    </div>
+                    <div class="flex-right-height">
+                      <p class="font-weight-bold mb-1">Total Revenue</p>
+                      <h2>45,383</h2>
+                      <p class="font-weight-medium p-0"><span class="text-success mr-2">+1.2%</span><span>growth</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-xl-3 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="icon-wrap">
+                      <i class="mdi mdi-file-document"></i>
+                    </div>
+                    <div class="flex-right-height">
+                      <p class="font-weight-bold mb-1">Total Player</p>
+                      <h2>65,758</h2>
+                      <p class="font-weight-medium p-0"><span class="text-success mr-2">+1.2%</span><span>growth</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-xl-3 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="icon-wrap">
+                      <i class="mdi mdi-account-outline"></i>
+                    </div>
+                    <div class="flex-right-height">
+                      <p class="font-weight-bold mb-1">Daily players</p>
+                      <h2>9,568</h2>
+                      <p class="font-weight-medium p-0"><span class="text-success mr-2">+1.2%</span><span>loss</span></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
