9f5d721278b2bc35baa9e0a374ab9834ab3f3c5f
kromate <akpananthony33@gmail.com>
2020-09-30 21:33:43 +0100

    worked on upload modal

---
+++ src/components/UploadModal.vue
@@ -0,0 +1,58 @@
+<template>
+  <div
+    v-if="show"
+    class="modal fade show"
+    id="exampleModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="exampleModalLabel"
+    style="display: block; background-color:#212529ba;"
+  >
+    <div class="modal-dialog modal-md" role="document">
+      <div class="modal-content">
+        <div class="modal-header text-center">
+          <div style="width:100%">
+            <h3 class="modal-title text-center">Select Upload Mode</h3>
+            <p class="warning">Please Ensure to save your question before switching Mode</p>
+          </div>
+
+          <button
+            type="button"
+            class="close"
+            @click="toggleModal"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body d-flex justify-content-between">
+          <div class="badge badge-outline-dark">Form</div>
+          <div class="badge badge-outline-primary">Spreadsheet</div>
+          <div class="badge badge-outline-success">local upload</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UploadModal",
+  props: ["show"],
+  methods: {
+    toggleModal() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
+
+<style>
+.warning {
+ font-size: 11.5px;
+    font-style: italic;
+    letter-spacing: 2px;
+    color: crimson;
+}
+</style>
--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,38 +1,68 @@
 <template>
   <div>
     <Menu></Menu>
+    <UploadModal :show="modal" @close="toggleModal"></UploadModal>
     <div class="center">
-      <a href="" class="float-left mt-2">Change</a>
+      <div class="badge badge-dark float-left mt-2"   @click="toggleModal">change</div>
       <div class="col-md-6 grid-margin stretch-card mt-3">
         <div class="card">
           <div class="card-body">
             <h4 class="card-title text-center">Upload form</h4>
-            <form class="forms-sample">
+            <form class="forms-sample" @submit.prevent="addQuestion">
               <div class="form-group">
                 <label for="question">Question</label>
                 <input
-                  @keyup="grow"
+                  v-model="questionInput"
                   type="text"
                   class="form-control"
                   id="question"
                   placeholder="Enter your question"
+                  required
                 />
               </div>
               <div class="form-group">
                 <label for="A">Option A</label>
-                <input type="Text" class="form-control ans" id="A" placeholder="Answer A" />
+                <input
+                  v-model="optionAInput"
+                  type="Text"
+                  class="form-control ans"
+                  id="A"
+                  placeholder="Answer A"
+                  required
+                />
               </div>
               <div class="form-group">
                 <label for="B">Option B</label>
-                <input type="text" class="form-control ans" id="B" placeholder="Answer B" />
+                <input
+                  v-model="optionBInput"
+                  type="text"
+                  class="form-control ans"
+                  id="B"
+                  placeholder="Answer B"
+                  required
+                />
               </div>
               <div class="form-group">
                 <label for="C">Option C</label>
-                <input type="text" class="form-control ans" id="C" placeholder="Answer c" />
+                <input
+                  v-model="optionCInput"
+                  type="text"
+                  class="form-control ans"
+                  id="C"
+                  placeholder="Answer c"
+                  required
+                />
               </div>
               <div class="form-group">
                 <label for="D">Option D</label>
-                <input type="text" class="form-control ans" id="D" placeholder="Answer D" />
+                <input
+                  v-model="optionDInput"
+                  type="text"
+                  class="form-control ans"
+                  id="D"
+                  placeholder="Answer D"
+                  required
+                />
               </div>
               <div class="center">
                 <button type="submit" class="btn btn-dark mr-2">Add</button>
@@ -43,139 +73,34 @@
       </div>
       <div class="my-5 p-5 card">
         <h4 class="text-center">Available Question on the list</h4>
-        <details class="card-header card">
-          <summary>1  <i class="mdi mdi-menu-down float-right" style="color:#1c273c"></i></summary>
+        <details class="card-header card" v-for="(question, index) in Questions" :key="index">
+          <summary>
+            {{index}}
+            <i class="mdi mdi-menu-down float-right" style="color:#1c273c"></i>
+          </summary>
           <div class="bo py-2">
             <div>
               <h6>Question</h6>
-              <p>Who killed adam ?</p>
+              <p>{{question.Question}}</p>
             </div>
             <div>
               <h6>Option A</h6>
-              <p>Google</p>
+              <p>{{question.OptionA}}</p>
             </div>
             <div>
               <h6>Option B</h6>
-              <p>Facebook</p>
+              <p>{{question.OptionB}}</p>
             </div>
             <div>
               <h6>Option C</h6>
-              <p>Microsoft</p>
+              <p>{{question.OptionC}}</p>
             </div>
             <div>
               <h6>Option D</h6>
-              <p>Amazon</p>
+              <p>{{question.OptionD}}</p>
             </div>
-             
-            </div>
+          </div>
         </details>
-        <details class="card-header card">
-          <summary>2  <i class="mdi mdi-menu-down float-right" style="color:#1c273c"></i></summary>
-          <div class="bo py-2">
-            <div>
-              <h6>Question</h6>
-              <p>Who killed adam ?</p>
-            </div>
-            <div>
-              <h6>Option A</h6>
-              <p>Google</p>
-            </div>
-            <div>
-              <h6>Option B</h6>
-              <p>Facebook</p>
-            </div>
-            <div>
-              <h6>Option C</h6>
-              <p>Microsoft</p>
-            </div>
-            <div>
-              <h6>Option D</h6>
-              <p>Amazon</p>
-            </div>
-             
-            </div>
-        </details>
-        <details class="card-header card">
-          <summary>3  <i class="mdi mdi-menu-down float-right" style="color:#1c273c"></i></summary>
-          <div class="bo py-2">
-            <div>
-              <h6>Question</h6>
-              <p>Who killed adam ?</p>
-            </div>
-            <div>
-              <h6>Option A</h6>
-              <p>Google</p>
-            </div>
-            <div>
-              <h6>Option B</h6>
-              <p>Facebook</p>
-            </div>
-            <div>
-              <h6>Option C</h6>
-              <p>Microsoft</p>
-            </div>
-            <div>
-              <h6>Option D</h6>
-              <p>Amazon</p>
-            </div>
-             
-            </div>
-        </details>
-        <details class="card-header card">
-          <summary>4  <i class="mdi mdi-menu-down float-right" style="color:#1c273c"></i></summary>
-          <div class="bo py-2">
-            <div>
-              <h6>Question</h6>
-              <p>Who killed adam ?</p>
-            </div>
-            <div>
-              <h6>Option A</h6>
-              <p>Google</p>
-            </div>
-            <div>
-              <h6>Option B</h6>
-              <p>Facebook</p>
-            </div>
-            <div>
-              <h6>Option C</h6>
-              <p>Microsoft</p>
-            </div>
-            <div>
-              <h6>Option D</h6>
-              <p>Amazon</p>
-            </div>
-             
-            </div>
-        </details>
-        <details class="card-header card">
-          <summary>5  <i class="mdi mdi-menu-down float-right" style="color:#1c273c"></i></summary>
-          <div class="bo py-2">
-            <div>
-              <h6>Question</h6>
-              <p>Who killed adam ?</p>
-            </div>
-            <div>
-              <h6>Option A</h6>
-              <p>Google</p>
-            </div>
-            <div>
-              <h6>Option B</h6>
-              <p>Facebook</p>
-            </div>
-            <div>
-              <h6>Option C</h6>
-              <p>Microsoft</p>
-            </div>
-            <div>
-              <h6>Option D</h6>
-              <p>Amazon</p>
-            </div>
-             
-            </div>
-        </details>
-      
- 
-     
       </div>
     </div>
   </div>
@@ -183,41 +108,82 @@
 
 <script>
 import Menu from "@/components/Menu";
+import UploadModal from "@/components/UploadModal";
 export default {
   components: {
-    Menu
+    Menu,
+    UploadModal
   },
   data() {
-    return {};
+    return {
+      modal:false,
+      questionInput:'',
+      optionAInput:'',
+      optionBInput:'',
+      optionCInput:'',
+      optionDInput:'',
+      Questions: [
+        {
+          "Question": "Who are you ?",
+          "OptionA": "Google",
+          "OptionB": "Microsoft",
+          "OptionC": "Amazon",
+          "OptionD": "Facebook"
+        }
+      ]
+    };
   },
   methods: {
-    grow(e) {
-      console.log(e.nextElementSibling);
+    toggleModal(){
+      this.modal = !this.modal
+    },
+
+    addQuestion(e){
+      e.preventDefault()
+      let questionObject = {
+          "Question": this.questionInput,
+          "OptionA": this.optionAInput,
+          "OptionB": this.optionBInput,
+          "OptionC": this.optionCInput,
+          "OptionD": this.optionDInput
+      }
+      this.Questions.push(questionObject)
+      this.clearForm()
+    },
+    clearForm(){
+          this.questionInput = ""
+          this.optionAInput = ""
+          this.optionBInput = ""
+          this.optionCInput = ""
+          this.optionDInput = ""
     }
   }
+
 };
 </script>
 
 <style scoped>
-.bo{
+.badge{
+  cursor:pointer
+}
+.bo {
   border-top: 1px solid rgba(0, 0, 0, 0.612);
 }
-details{
-  width:17rem;
+details {
+  width: 17rem;
   border: 1px solid rgba(0, 0, 0, 0.224);
   margin: 2px 0px;
 }
-summary{
+summary {
   outline: none;
-
 }
 .center {
   display: flex;
   flex-direction: column;
   /* justify-content: center; */
-  align-items: center
+  align-items: center;
 }
-.ans{
+.ans {
   border-color: #1c273c !important;
 }
 details > summary::-webkit-details-marker {
